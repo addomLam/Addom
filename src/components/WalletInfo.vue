4da23297667fa8e8eb9cd787a1a357dc28d64c51
lamlauchiu <lamlauchiu@lamlauchius-MacBook-Pro.local>
2018-04-26 00:19:38 +0800

    uplaod all

---
+++ src/components/WalletInfo.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-flex xs6>
+    <v-card class="card--flex-toolbar">
+      <v-toolbar card class="light-blue">
+        <v-toolbar-title class="white--text">My Wallet</v-toolbar-title>
+      </v-toolbar>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title>
+            Address
+          </v-list-tile-title>
+          <v-list-tile-content>
+            {{ coinbase }}
+          </v-list-tile-content>
+        </v-list-tile>
+  
+        <v-list-tile>
+          <v-list-tile-title>
+            Ether Balance
+          </v-list-tile-title>
+          <v-list-tile-content>
+            {{ balance }}
+          </v-list-tile-content>
+        </v-list-tile>
+  
+        <v-list-tile>
+          <v-list-tile-title>
+            SPEEDCoin Balance
+          </v-list-tile-title>
+          <v-list-tile-content>
+            {{ tokens }}
+          </v-list-tile-content>
+        </v-list-tile>
+  
+      </v-list>
+  
+    </v-card>
+  </v-flex>
+</template>
+<script>
+import { CONTRACT } from '../contract'
+
+export default {
+  data () {
+    return {
+      coinbase: 0x00,
+      balance: 0,
+      tokens: 0
+    }
+  },
+
+  mounted () {
+    const owner = this;
+    this.coinbase = CONTRACT._eth.getCoinbase(function(err, cb) { 
+      owner.coinbase = cb;
+      owner.getEtherBalance();
+      owner.getTokenBalance();
+    });
+
+    CONTRACT.Transfer((err, res) => {
+      this.getTokenBalance()
+    });
+
+  },
+
+  methods: {
+    getEtherBalance () {
+      CONTRACT._eth.getBalance(this.coinbase, (err, bal) => {
+        if (!err) {
+          this.balance = web3.fromWei(bal, 'ether').toNumber()
+        }
+        console.log(err)
+      })
+    },
+
+    getTokenBalance () {
+      CONTRACT.balanceOf(this.coinbase, (err, tkns) => {
+        if (!err) {
+          this.tokens = web3.fromWei(tkns, 'ether').toNumber()
+        }
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
